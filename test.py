239757fc74bfef2384f9c67b749de87c769f8f1a
fero29 <fero.masiar@gmail.com>
2021-02-16 22:48:23 +0100

    das

--- test.py
+++ test.py
@@ -8,7 +8,9 @@ while(True):
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+    print(frame.shape)
 
     # Display the resulting frame
     cv2.imshow('frame',frame)
